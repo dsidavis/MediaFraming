c4c8d3ff7b82c02f386869c2679cea3709c1c295
Duncan Temple Lang <duncan@r-project.org>
2017-12-08 15:24:01 -0800

    getting this to work

--- Plotly/fix.R
+++ Plotly/fix.R
@@ -1,14 +1,23 @@
+library(RJSONIO)
+library(XML)
 
-doc = htmlParse(hfile)
-h = getNodeSet(doc, "//head")
+fix = 
+function(hfile, out = NA, doc = htmlParse(hfile), houses = c("ABC", "FOX"))
+{
+    h = getNodeSet(doc, "//head")[[1]]
 
-newXMLNode("script", attrs = c(src = "poll.js"), parent = h)
-
-houses = c("QUINN", "SRBI")
-
-def = sprintf("var HouseNames = %s;", toJSON(houses))
-newXMLNode("script", def, attrs = c(type = "javascript"), parent = h)
+    # may want the type attribute.
+    newXMLNode("script", attrs = c(src = "poll.js"), parent = h)
 
 
-div = getNodeSet(doc, "//div[@class = 'htmlwidget_container']")[[1]]
-newXMLNode("input", atts = c(type = "button", value = "toggle polling data", onclick = "togglePolling()"), parent = div) 
+    def = sprintf("var HouseNames = %s;", toJSON(houses))
+    newXMLNode("script", def, attrs = c(type = "text/javascript"), parent = h)
+
+    div = getNodeSet(doc, "//div[@id = 'htmlwidget_container']")[[1]]
+    newXMLNode("input", attrs = c(type = "button", value = "toggle polling data", onclick = "togglePolling()"), parent = div)
+
+    if(!is.na(out))
+        saveXML(doc, out)
+    else
+        doc
+}
--- Plotly/poll.js
+++ Plotly/poll.js
@@ -1,12 +1,36 @@
+
+function NSResolver (nsPrefix)
+{
+    if (nsPrefix == "svg") {
+        return "http://www.w3.org/2000/svg"
+    }
+    return null;
+}
+
 function toggleHouse (house)
 {
-    var el = document.evaluate("//text[. = '" + house + "']/following-sibling::rect", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    var ev = new Event('click');
-    el.dispatchEvent(ev);
+    // /following-sibling::svg:rect
+    var el = document.evaluate("//svg:text[. = '" + house + "']", document, NSResolver, XPathResult.ANY_TYPE, null);
+    var tmp = el.iterateNext();
+    alert("event: " + house + ' ' + tmp + ' ' + tmp.textContent + ' ');
+    if(tmp) {
+	tmp =  tmp.nextSibling;
+	var ev = new Event('plotly_event');
+	tmp.dispatchEvent(ev);
+    }
 }
+
+var pollingOn = true;
 
 function togglePolling()
 {
-    for(i in HouseNames)
-	toggleHouse(i);
+//    for(i=0; i < HouseNames.length; i++)
+//	toggleHouse(HouseNames[i]);
+
+    // now toggle the dashed line.
+    var el = document.evaluate("//svg:path[@class='js-line' and contains(@style, 'rgb(0, 0, 0)')]", document, NSResolver, XPathResult.ANY_TYPE, null);
+    el = el.iterateNext();
+    alert("line: " + el + ' pollingOn = ' + pollingOn);
+    el.setAttribute('visibility', pollingOn ? 'hidden' : 'visible');
+    pollingOn = !pollingOn;
 }

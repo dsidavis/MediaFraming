f7f4b56ff7ab16f3051e11b5c8d380d825a684ee
Duncan Temple Lang <duncan@r-project.org>
2017-12-08 15:39:48 -0800

    toggling the points groups

--- Plotly/poll.js
+++ Plotly/poll.js
@@ -22,20 +22,39 @@ function toggleHouse (house)
 
 var pollingOn = true;
 
+function findPollingPoints()
+{
+    xp = "//svg:g[contains(@class, 'trace ')]//svg:g[@class='points' and count(./svg:path) > 0]";
+    var nodes = document.evaluate(xp, document, NSResolver, XPathResult.ANY_TYPE, null);
+    return(nodes);
+}
+
 function togglePolling()
 {
 //    for(i=0; i < HouseNames.length; i++)
 //	toggleHouse(HouseNames[i]);
 
+    var nodes = findPollingPoints();
+    var el;
+    el = nodes.iterateNext();
+    var els = [];
+    while(el) {
+	els.push(el);
+	el = nodes.iterateNext();
+    }
+    // alert("number of nodes: " + els.length);
+    for(i=0; i < els.length; i++) {
+	els[i].setAttribute('visibility', pollingOn ? 'hidden' : 'visible');	
+    }
     // now toggle the dashed line.
     toggleLine();
+    pollingOn = !pollingOn;    
 }
 
 function toggleLine()
 {
     var el = document.evaluate("//svg:path[@class='js-line' and contains(@style, 'rgb(0, 0, 0)')]", document, NSResolver, XPathResult.ANY_TYPE, null);
     el = el.iterateNext();
-    alert("line: " + el + ' pollingOn = ' + pollingOn);
+//    alert("line: " + el + ' pollingOn = ' + pollingOn);
     el.setAttribute('visibility', pollingOn ? 'hidden' : 'visible');
-    pollingOn = !pollingOn;
 }
